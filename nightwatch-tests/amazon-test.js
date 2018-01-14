6ddefdf9b3750eb33d9520460ec54952c8a119ca
Jon Holewa <jonholewa@outlook.com>
2018-01-13 18:10:38 -0800

    nightwatch now searches for shoes and navigates to the results page

--- nightwatch-tests/amazon-test.js
+++ nightwatch-tests/amazon-test.js
@@ -1,8 +1,15 @@
 module.exports = {
-    'Login test': function (client) {
+    'Amazon Test': function (client) {
         client
             .url('https://www.amazon.com/')
             .assert.urlEquals('https://www.amazon.com/')
+            .waitForElementVisible('#twotabsearchtextbox', 2000)
+            .click('#twotabsearchtextbox')
+            .setValue('#twotabsearchtextbox', 'nike shoes')
+            .click('.nav-search-submit > input.nav-input')
+            .pause(1000)
+            //.assert.url('https://www.amazon.com/')
+            .pause(3000)
             .end();
     }
 };
